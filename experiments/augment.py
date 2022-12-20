c7ace4e78b251beaa383d321626aa7f2a00430a3
phj6688 <peyman.h.j@gmail.com>
2022-12-20 17:21:13 +0100

    new augmented files

--- experiments/augment.py
+++ experiments/augment.py
@@ -3,14 +3,14 @@ from functions import *
 from tqdm import tqdm
 
 datasets = ['pc','cr','subj']
-#aug_methods = ['eda_augmenter','wordnet_augmenter','aeda_augmenter','backtranslation_augmenter','clare_augmenter']
-aug_methods = ['backtranslation_augmenter','clare_augmenter']
-n_samples = [1,2,4,8,10]
+aug_methods = ['eda_augmenter','wordnet_augmenter','aeda_augmenter','backtranslation_augmenter','clare_augmenter']
+#aug_methods = ['backtranslation_augmenter','clare_augmenter']
+n_samples = [1,2,4,8]
 
-
+# modify the path to your data (traub1000_sample.txt or train.txt or ...)
 
 def aug_samples(dataset_name, aug_method, n_sample,org=True):
-    path = f'data/{dataset_name}/train.txt'
+    path = f'data/{dataset_name}/train1000_sample.txt'
     data = load_data(path)
     
     
@@ -18,10 +18,11 @@ def aug_samples(dataset_name, aug_method, n_sample,org=True):
                     label_column='class',target_column='text',include_original=org)
 
     augmented_data = augmented_data[['class','text']]
-    augmented_data.to_csv(f'data/{dataset_name}/train_{aug_method}_{n_sample}.csv',index=False)
+    augmented_data.to_csv(f'data/{dataset_name}/train_{aug_method}_{n_sample}_includeOrg_{org}.csv',index=False)
 
-    np.savetxt(f'data/{dataset_name}/train_{aug_method}_{n_sample}.txt', augmented_data.values, fmt='%s', delimiter='\t')
-    print(f'{dataset_name} {aug_method} {n_sample} done')
+    np.savetxt(f'data/{dataset_name}/train_{aug_method}_{n_sample}_includeOrg_{org}.txt', augmented_data.values, fmt='%s', delimiter='\t')
+    print(f'{dataset_name} {aug_method} {n_sample} include original {org} done')
+    
 
 if __name__ == '__main__':
     for dataset in tqdm(datasets):
